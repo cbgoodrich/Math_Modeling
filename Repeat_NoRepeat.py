9b5673bb65462a7f91e948789a63377f5445806d
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-02 10:08:25 -0400

    Updated from Brython Server: 10/2/2017 10:08:24 AM

--- Repeat_NoRepeat.py
+++ Repeat_NoRepeat.py
@@ -1,1 +1,16 @@
+#Charlie Goodrich
+#10/02/17
+#Repeat_NoRepeat - inputs numberator and denominator, tells you # of repeating numbers
+
+numerator = int(input("Enter a numerator value: "))
+denominator = int(input("Enter a denominator value: "))
+
+frac = numerator / denominator
+
+frac1 = str(frac)
+
+print(frac)
+
+count = frac1.count("3")
+print(count)
 
