78f98c39e7955d08fbacd8e2011568f4a63eb076
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-02 12:07:09 -0400

    Updated from Brython Server: 10/2/2017 12:07:08 PM

--- Repeat_NoRepeat.py
+++ Repeat_NoRepeat.py
@@ -11,6 +11,46 @@ frac1 = str(frac)
 
 print(frac)
 
-count = frac1.count("3")
-print(count)
+if frac1.count("1") > 1:
+    a = 1
+else:
+    a = 0
+if frac1.count("2") > 1:
+    b = 1
+else:
+    b = 0
+if frac1.count("3") > 1:
+    c = 1
+else:
+    c = 0
+if frac1.count("4") > 1:
+    d = 1
+else:
+    d = 0
+if frac1.count("5") > 1:
+    e = 1
+else:
+    e = 0
+if frac1.count("6") > 1:
+    f = 1
+else:
+    f = 0
+if frac1.count("7") > 1:
+    g = 1
+else:
+    g = 0
+if frac1.count("8") > 1:
+    h = 1
+else:
+    h = 0
+if frac1.count("9") > 1:
+    j = 1
+else:
+    j = 0
+if frac1.count("0") > 1:
+    i = 1
+else:
+    i = 0
+print("You have",a + b + c + d + e + f + g + h + i, "repeating digits")
+
 
