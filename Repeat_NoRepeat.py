2e0daeb8c92d425454c7c14d03b15c7a3735d09b
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-02 12:51:13 -0400

    Updated from Brython Server: 10/2/2017 12:51:13 PM

--- Repeat_NoRepeat.py
+++ Repeat_NoRepeat.py
@@ -14,64 +14,65 @@ print(frac)
 if frac1.count("1") > 1:
     a = 1
 elif frac1.count("1") == 1:
-    A = .5
+    z = 1
 else:
     a = 0
 if frac1.count("2") > 1:
     b = 1
 elif frac1.count("2") == 1:
-    B = .5
+    y = 1
 else:
     b = 0
 if frac1.count("3") > 1:
     c = 1
 elif frac1.count("3") == 1:
-    C = .5
+    x = 1
 else:
     c = 0
 if frac1.count("4") > 1:
     d = 1
-elif frac1.count("4") == 1:
-    D = .5
+if frac1.count("4") == 1:
+    w = 1
 else:
     d = 0
 if frac1.count("5") > 1:
     e = 1
-elif frac1.count("6") == 1:
-    E = .5
+elif frac1.count("5") == 1:
+    v = 1
 else:
     e = 0
 if frac1.count("6") > 1:
     f = 1
-elif frac1.count("7") == 1:
-    F = .5
+elif frac1.count("6") == 1:
+    u = 1
 else:
     f = 0
 if frac1.count("7") > 1:
     g = 1
 elif frac1.count("7") == 1:
-    G = .5
+    t = 1
 else:
     g = 0
 if frac1.count("8") > 1:
     h = 1
-elif frac1.count("9") == 1:
-    H = .5
+elif frac1.count("8") == 1:
+    s = 1
 else:
     h = 0
 if frac1.count("9") > 1:
-    j = 1
-elif frac1.count("9") == 1:
-    J = .5
-else:
-    j = 0
-if frac1.count("0") > 1:
     i = 1
-elif frac1.count("0") == 1:
-    I = .5
+elif frac1.count("9") == 1:
+    r = 1
 else:
     i = 0
-print("You have", a + b + c + d + e + f + g + h + i, "repeating digits")
-print("You have", A + B + C + D + E + F + G + H + I, "non-repeating digits")
+if frac1.count("0") > 1:
+    j = 1
+elif frac1.count("0") == 1:
+    q = 1
+else:
+    j = 0
+print("You have", a + b + c + d + e + f + g + h + i + j, "repeating digits")
+
+
 
 
