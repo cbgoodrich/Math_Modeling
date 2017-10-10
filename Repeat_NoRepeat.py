b7f38a3ffccc9f05a7d4764c2325e648767cdff7
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-10 12:10:00 -0400

    Updated from Brython Server: 10/10/2017 12:09:59 PM

--- Repeat_NoRepeat.py
+++ Repeat_NoRepeat.py
@@ -13,6 +13,7 @@ i = 1
 counterOf2 = 0 #the counter of factors of two in the denominator
 counterOf5 = 0 #the counter of factors of five in the denominator
 counterOf3 = 0
+counterOf7 = 0
 """ since the denominator is limited to 100, dividing through
 by all the powers of 2 less than 100"""
 while i <= 6: 
@@ -27,10 +28,24 @@ while i <= 2: #same thing with the powers of two above, doing the same with 5
     i += 1
 i = 1
 
-denominator_2 = denominator / (2 ** counterOf2)
-denominator_prime = denominator_2 / (5 ** counterOf5)
+denominator_2 = denominator / (2 ** counterOf2) """finding what's left of the
+denominator after diving through by the powers of two that go into it"""
 
-print(denominator_prime)
+denominator_prime = denominator_2 / (5 ** counterOf5) """finding the end
+denominator after dividing through by 2 and 5"""
+
+while i < 4: #doing the powers thing with 3
+    if denominator_prime % (3 ** i) == 0:
+        counterOf3 += 1
+    i += 1
+i = 1
+
+while i < 2: #doing the powers thing with 7
+    if denominator_prime % (7 ** i) == 0:
+        counterOf7 += 1
+    i += 1
+
+
 
 
 
