4cf8c4befd85c3c5ff4c04b536fb4e8f7aa41930
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-10 13:37:26 -0400

    Updated from Brython Server: 10/10/2017 1:37:25 PM

--- Repeat_NoRepeat.py
+++ Repeat_NoRepeat.py
@@ -3,7 +3,7 @@
 #Repeat_NoRepeat - inputs numberator and denominator, tells you # of repeating numbers
 
 numerator = int(input("Enter a numerator value equal to 1: "))
-denominator = int(input("Enter a denominator value between 1 and 100: "))
+denominator = int(input("Enter a denominator value between 1 and 50: "))
 
 frac = numerator / denominator
 
@@ -14,9 +14,20 @@ counterOf2 = 0 #the counter of factors of two in the denominator
 counterOf5 = 0 #the counter of factors of five in the denominator
 counterOf3 = 0
 counterOf7 = 0
+counterOf11 = 0
+counterOf13 = 0
+counterOf17 = 0
+counterOf19 = 0
+counterOf23 = 0
+counterOf29 = 0
+counterOf31 = 0
+counterOf37 = 0
+counterOf41 = 0
+counterOf43 = 0
+counterOf47 = 0
 """ since the denominator is limited to 100, dividing through
-by all the powers of 2 less than 100"""
-while i <= 6: 
+by all the powers of 2 less than 50"""
+while i <= 5: 
     if denominator % (2 ** i) == 0:
         counterOf2 += 1
     i += 1
@@ -28,32 +39,133 @@ while i <= 2: #same thing with the powers of two above, doing the same with 5
     i += 1
 i = 1
 
-denominator_2 = denominator / (2 ** counterOf2) """finding what's left of the
-denominator after diving through by the powers of two that go into it"""
+denominator_2 = denominator / (2 ** counterOf2) 
+"""finding what's left of the denominator after diving through 
+by the powers of two that go into it"""
 
-denominator_prime = denominator_2 / (5 ** counterOf5) """finding the end
-denominator after dividing through by 2 and 5"""
+denominator_prime = denominator_2 / (5 ** counterOf5) 
+#finding the enddenominator after dividing through by 2 and 5
 
-while i < 4: #doing the powers thing with 3
+while i <= 3: #doing the powers thing with 3
     if denominator_prime % (3 ** i) == 0:
         counterOf3 += 1
     i += 1
 i = 1
 
-while i < 2: #doing the powers thing with 7
+while i <= 2: #doing the powers thing with 7
     if denominator_prime % (7 ** i) == 0:
         counterOf7 += 1
     i += 1
+i = 1
+
+while i < 2: #now doing with 11
+    if denominator_prime % (11 ** i) == 0:
+        counterOf11 += 1
+    i += 1
+i = 1
+
+while i < 2: #now doing with 13
+    if denominator_prime % (13 ** i) == 0:
+        counterOf13 += 1
+    i += 1
+i = 1
+
+while i < 2: #now doing with 17
+    if denominator_prime % (17 ** i) == 0:
+        counterOf17 += 1
+    i += 1
+i = 1
+
+while i < 2: #now doing with 19
+    if denominator_prime % (19 ** i) == 0:
+        counterOf19 += 1
+    i += 1
+i = 1
+
+while i < 2: #now doing with 23
+    if denominator_prime % (23 ** i) == 0:
+        counterOf23 += 1
+    i += 1
+i = 1
+
+while i < 2: #now doing with 29
+    if denominator_prime % (29 ** i) == 0:
+        counterOf29 += 1
+    i += 1
+i = 1
+
+while i < 2: #now doing with 31
+    if denominator_prime % (31 ** i) == 0:
+        counterOf31 += 1
+    i += 1
+i = 1
+
+while i < 2: #now doing with 37
+    if denominator_prime % (37 ** i) == 0:
+        counterOf37 += 1
+    i += 1
+i = 1
+
+while i < 2: #now doing with 41
+    if denominator_prime % (41 ** i) == 0:
+        counterOf41 += 1
+    i += 1
+i = 1
+
+while i < 2: #now doing with 43
+    if denominator_prime % (43 ** i) == 0:
+        counterOf43 += 1
+    i += 1
+i = 1
+
+while i < 2: #now doing with 47
+    if denominator_prime % (47 ** i) == 0:
+        counterOf47 += 1
+    i += 1
+i = 1
 
 
-
-
-
-
-
-
-
-"""if counterOf2 >= counterOf5:
+if counterOf2 >= counterOf5:
     print("You have", counterOf2, "non repeating digits")
 else:
-    print("You have", counterOf5, "non repeating digits")"""
+    print("You have", counterOf5, "non repeating digits")
+
+if counterOf47 >= 1:
+    print("You have at most 46 repeating digits")
+elif counterOf43 >= 1:
+    print("You have at most 42 repeating digits")
+elif counterOf41 >= 1:
+    print("You have at most 40 repeating digits")
+elif counterOf37 >= 1:
+    print("You have at most 36 repeating digits")
+elif counterOf31 >= 1:
+    print("You have at most 30 repeating digits")
+elif counterOf29 >= 1:
+    print("You have at most 28 repeating digits")
+elif counterOf23 >= 1:
+    print("You have at most 22 repeating digits")
+elif counterOf19 >= 1:
+    print("You have at most 18 repeating digits")
+elif counterOf17 >= 1:
+    print("You have at most 16 repeating digits")
+elif counterOf13 >= 1:
+    print("You have at most 12 repeating digits")
+elif counterOf11 >= 1:
+    print("You have at most 10 repeating digits")
+elif counterOf7 >= 1:
+    print("You have 6 repeating digits")
+elif counterOf3 >=1:
+    print("You have 1 repeating digit")
+
+
+
+
+
+
+
+
+
+
+
+
+
