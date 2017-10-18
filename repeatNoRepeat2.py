128e4815b7c60845fe81919ce9809f8042c1ebf1
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-18 13:08:24 -0400

    Updated from Brython Server: 10/18/2017 1:08:23 PM

--- repeatNoRepeat2.py
+++ repeatNoRepeat2.py
@@ -1,1 +1,13 @@
+#Charlie Goodrich
+#10/18/17
+#repeatNoRepeat2.py - tells you the fraction
 
+repeat = int(input("Enter the number of repeating digits: "))
+
+frac = 1/3
+
+num1 = frac * 10**repeat
+num2 = num1//1
+num3 = 10**repeat - 1
+
+print(num2/num3)
